5908901a0bf0707e3b4b8a33e93787a56f5fc010
Amrishkhan Sheik Abdullah <amrishkhan.abdullah@techcarrot.ae>
2025-08-19 15:19:22 +0400

    Implement multiple code changes to enhance functionality and improve performance

--- src/Migration/1755158527137-TransitionServiceChanges.ts
+++ src/Migration/1755158527137-TransitionServiceChanges.ts
@@ -4,15 +4,15 @@ export class TransitionServiceChanges1755158527137 implements MigrationInterface
     name = 'TransitionServiceChanges1755158527137'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`move_in_requests\` CHANGE \`status\` \`status\` enum ('new', 'rfi-pending', 'rfi-submitted', 'approved', 'user-cancelled', 'cancelled', 'closed') NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`move_out_requests\` CHANGE \`status\` \`status\` enum ('new', 'rfi-pending', 'rfi-submitted', 'approved', 'user-cancelled', 'cancelled', 'closed') NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`move_in_request_logs\` CHANGE \`status\` \`status\` enum ('new', 'rfi-pending', 'rfi-submitted', 'approved', 'user-cancelled', 'cancelled', 'closed') NOT NULL`);
+        // await queryRunner.query(`ALTER TABLE \`move_in_requests\` CHANGE \`status\` \`status\` enum ('new', 'rfi-pending', 'rfi-submitted', 'approved', 'user-cancelled', 'cancelled', 'closed') NOT NULL`);
+        // await queryRunner.query(`ALTER TABLE \`move_out_requests\` CHANGE \`status\` \`status\` enum ('new', 'rfi-pending', 'rfi-submitted', 'approved', 'user-cancelled', 'cancelled', 'closed') NOT NULL`);
+        // await queryRunner.query(`ALTER TABLE \`move_in_request_logs\` CHANGE \`status\` \`status\` enum ('new', 'rfi-pending', 'rfi-submitted', 'approved', 'user-cancelled', 'cancelled', 'closed') NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`move_in_request_logs\` CHANGE \`status\` \`status\` enum ('open', 'rfi', 'user-cancelled', 'cancel', 'approve') NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`move_out_requests\` CHANGE \`status\` \`status\` enum ('open', 'rfi', 'user-cancelled', 'cancel', 'approve') NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`move_in_requests\` CHANGE \`status\` \`status\` enum ('open', 'rfi', 'user-cancelled', 'cancel', 'approve') NOT NULL`);
+        // await queryRunner.query(`ALTER TABLE \`move_in_request_logs\` CHANGE \`status\` \`status\` enum ('open', 'rfi', 'user-cancelled', 'cancel', 'approve') NOT NULL`);
+        // await queryRunner.query(`ALTER TABLE \`move_out_requests\` CHANGE \`status\` \`status\` enum ('open', 'rfi', 'user-cancelled', 'cancel', 'approve') NOT NULL`);
+        // await queryRunner.query(`ALTER TABLE \`move_in_requests\` CHANGE \`status\` \`status\` enum ('open', 'rfi', 'user-cancelled', 'cancel', 'approve') NOT NULL`);
     }
 
 }
