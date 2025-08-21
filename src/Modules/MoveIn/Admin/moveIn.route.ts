6535183a35b0f6001694275369402ce124689100
krishnanKhannan <126442951+krishnanKhannan@users.noreply.github.com>
2025-08-21 15:54:53 +0400

    Feature/move in (#6)
    * Move in get Api changes
    
    * Move In Get All details
    
    * Changes
    
    * Move In changes
    
    * refactor: Simplify route definitions in moveIn.route.ts
    
    * refactor: Clean up route definitions in moveIn.route.ts
    
    * Code review fixes
    
    * refactor: Remove unused imports and clean up code structure in moveIn.route.ts
    
    * refactor: Simplify response handling and clean up code in MoveIn controllers and services
    
    * Chnages
    
    ---------
    
    Co-authored-by: Amrishkhan Sheik Abdullah <amrishkhan.abdullah@techcarrot.ae>

--- src/Modules/MoveIn/Admin/moveIn.route.ts
+++ src/Modules/MoveIn/Admin/moveIn.route.ts
@@ -16,3 +16,48 @@ router.get("/request", auth.auth(), validate(moveInValidation.getAdminMoveIn), c
 router.get('/moveInDetails/:requestId', auth.auth(), validate(moveInValidation.getAdminMoveInDetails), catchAsync(moveInController.getAllMoveInDetailsList));
 
 export default router;
+
+// Swagger documentation
+/**
+* @swagger
+* tags:
+*   - name: MoveIn
+*     description: MoveIn management
+*/
+
+/**
+* @swagger
+* /admin/move-in/request:
+*   get:
+*     summary: Close a move in request
+*     tags: [MoveIn]
+*     responses:
+*       200:
+*         description: Move in request
+*/
+
+// Swagger documentation
+/**
+* @swagger
+* tags:
+*   - name: MoveOut
+*     description: MoveOut management
+*/
+
+/**
+* @swagger
+* /admin/movein/moveInDetails/{requestId}:
+*   get:
+*     summary: Close a move in request
+*     tags: [MoveOut]
+*     parameters:
+*       - in: path
+*         name: requestId
+*         required: true
+*         description: The ID of the move out request
+*    
+*     responses:
+*       200:
+*         description: Move out request closed
+*/
+
--- src/Modules/MoveIn/Mobile/moveIn.route.ts
+++ src/Modules/MoveIn/Mobile/moveIn.route.ts
@@ -15,3 +15,41 @@ router.get('/request', auth.auth(), validate(moveInValidation.getMobileMoveIn), 
 router.get('/request/:unitId', auth.auth(), validate(moveInValidation.getMobileMoveIn), catchAsync(moveInController.getAllMoveInRequestList));
 
 export default router;
+
+// Swagger documentation
+/**
+* @swagger
+* tags:
+*   - name: MoveIn
+*     description: MoveIn management
+*/
+
+/**
+* @swagger
+* /admin/move-in/request:
+*   get:
+*     summary: Close a move in request
+*     tags: [MoveIn]
+*     responses:
+*       200:
+*         description: Move in request 
+*/
+
+/**
+* @swagger
+* /admin/movein/request/{unitId}:
+*   get:
+*     summary: get a move in request List
+*     tags: [MoveOut]
+*     parameters:
+*       - in: path
+*         name: unitId
+*         required: true
+*         description: The ID of the move out request
+*    
+*     responses:
+*       200:
+*         description: Move out request closed
+*/
+
+
